612ec37f999f972326f93e5b24f68721adf0571a
Alaurosa <55215894+Alaurosa@users.noreply.github.com>
2019-11-10 17:11:16 -0800

    Update Frame2.js

--- Frame2.js
+++ Frame2.js
@@ -8,5 +8,5 @@ function printHTML(input){
 }
 
 function one() {
-  document.getElementById("response1").innerHTML = `As you enter the dark and barely lit warehouse you see two doors, one to your left and one to your right.   <h3>Do you want to 1. Go through the door on the left in the warehouse or 2. Go through the door on the right? </h3><button type="button" name="Theevilbutton" onClick="firstAThing()">1</button><button type="button" name="Theevilbutton2" onClick="firstBThing()">2</button>';
+  document.getElementById("response1").innerHTML = `As you enter the dark and barely lit warehouse you see two doors, one to your left and one to your right.   <h3>Do you want to 1. Go through the door on the left in the warehouse or 2. Go through the door on the right? </h3><button type="button" name="Theevilbutton" onClick="one()">1</button><button type="button" name="Theevilbutton2" onClick="two()">2</button>';
 }
